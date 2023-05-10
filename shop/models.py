b49519418e4614933ae86cf88ccc12c5c8ebf792
Nick Fernandez <maxim.6.vizgert@gmail.com>
2023-05-10 17:46:13 +0300

    10.05.2023

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
+
+class UserProfils(models.Model):
+
+    user = models.ForeignKey(User, verbose_name="Пользователь", on_delete=models.CASCADE)
+    phone = PhoneNumberField(blank=True)
+    
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+
 
 # Create your views here.
 
 def indexpage(request):
     return render(request, 'shop/index.html')
+
+
+def loginuser(request):
+
+    if request.method.GET:
+        return ''
